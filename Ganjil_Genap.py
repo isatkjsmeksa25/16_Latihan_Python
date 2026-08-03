637be262be4d2bde86f7b19324fdd1b033860a1a
isatkjsmeksa25 <isa.tkjsmeksa25@gmail.com>
2026-08-03 09:37:40 +0700

    Ganjil Genap

---
+++ Ganjil_Genap.py
@@ -0,0 +1,7 @@
+"Menentukan Apakah Angka Ganjil atau Genap"
+
+x = int(input("Silahkan Masukkan Angka: "))
+if x % 2 == 0:
+	print("Angka", x, "adalah Bilangan Genap")
+else:
+	print("Angka", x, "adalah Bilangan Ganjil")
